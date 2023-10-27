57e3c0db67ca8bf21215fe07c13240a3c18a1381
Trevor Vance <trevorvance525@gmail.com>
2023-10-27 14:41:09 +0000

    Updated README.md Renamed project.

--- ASFSteamScreamFest2023/ASFSteamScreamFest2023.cs
+++ ASFSteamScreamFest2023/ASFSteamScreamFest2023.cs
@@ -1,16 +1,16 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using ArchiSteamFarm.Core;
 using ArchiSteamFarm.Plugins.Interfaces;
 using JetBrains.Annotations;
 
-namespace MyAwesomePlugin;
+namespace ASFSteamScreamFest2023;
 
 #pragma warning disable CA1812 // ASF uses this class during runtime
 [UsedImplicitly]
-internal sealed class MyAwesomePlugin : IPlugin {
-	public string Name => nameof(MyAwesomePlugin);
-	public Version Version => typeof(MyAwesomePlugin).Assembly.GetName().Version ?? throw new InvalidOperationException(nameof(Version));
+internal sealed class ASFSteamScreamFest2023 : IPlugin {
+	public string Name => nameof(ASFSteamScreamFest2023);
+	public Version Version => typeof(ASFSteamScreamFest2023).Assembly.GetName().Version ?? throw new InvalidOperationException(nameof(Version));
 
 	public Task OnLoaded() {
 		ASF.ArchiLogger.LogGenericInfo($"Hello {Name}!");
